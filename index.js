f377cab214bc6518cf9f6be5095ebb53a9b48b0e
Blake Gilbertson <blakegilbertson@protonmail.com>
2019-07-29 19:04:41 +0100

    Updating mixed use of modules to use CommonJS modules

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ app.get('/john', function(req, res) {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-export default app
+module.exports = app;
--- index.spec.js
+++ index.spec.js
@@ -1,22 +1,16 @@
-// import app from './index'
-
 const request = require('supertest')
-const express = require('express')
-const app = express()
+const app = require('./index')
 
-app.get('/blake', function(req, res) {
-  res.status(200).json({ name: 'blake' });
-});
-
-app.get('/john', function(req, res) {
-  res.status(200).json({ name: 'john' });
-});
 
 describe('Test a 200 on routes', () => {
+
+  // As an FYI, I intentionally built these two differently 
+  //   - the first based on the express tut and the second on MP
+
   test('It should respond with a 200 status and be json for /blake route', async () => {
     request(app)
       .get('/blake')
-      .expect('Content-Type', /json/)
+      // .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, res) {
         if (err) throw err;
