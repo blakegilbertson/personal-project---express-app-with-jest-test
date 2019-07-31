908083cb78f67172b23d772403a579bec6647af2
Blake Gilbertson <blakegilbertson@protonmail.com>
2019-07-31 14:09:08 +0100

    Changing start script and playing with routes/middleware

--- express-app/app.js
+++ express-app/app.js
@@ -5,15 +5,22 @@ const app = express()
 const router = express.Router()
 const port = 3000
 
+console.log('router: ', router)
+
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+})
+
+
 // const { blakeDidThis, blakeAlsoDidThis } = require('../middleware/blake')
 import { blakeDidThis, blakeAlsoDidThis } from '../middleware/blake'
 
 const john = require('../middleware/john')
 
-app.get(
-  '/', 
-  (req, res) => res.send('Hello World!')
-)
+// app.get(
+//   '/', 
+//   (req, res) => res.send('Hello World!')
+// )
 
 app.get(
   '/blake',
@@ -21,10 +28,63 @@ app.get(
   blakeAlsoDidThis
 )
 
+
+
+// router.get(
+//   '/blake',
+//   blakeDidThis,
+//   blakeAlsoDidThis
+// )
+
+// router.get('/something', function (req, res) {
+//   res.send('HEEEEEYYY :', req.params);
+// });
+
 app.get(
   '/john', 
   john
 );
+
+// not linked to a specific route, runs for all
+app.use(function (req, res, next) {
+  console.log('Current Time:', Date.now())
+  next()
+})
+
+app.use(
+  '/book/:id', function(req, res, next) {
+    console.log('/book/:id request type: ', req.method);
+    next()
+  }
+)
+
+// testing next('route') when user :id is 0
+app.get('/user/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next route
+  if (req.params.id === '0') next('route')
+  // otherwise pass the control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // send a regular response
+  res.send('regular')
+})
+
+// handler for the /user/:id path, which sends a special response
+app.get('/user/:id', function (req, res, next) {
+  res.send('special')
+})
+
+
+// doesnt work, why tho?
+router.use(function (req, res, next) {
+  console.log('Blake')
+  next()
+})
+
+
+
+
+
 
 // found here, works: https://expressjs.com/en/guide/routing.html
 // todo - investigate the differences between this method and the above
@@ -35,6 +95,28 @@ app.get(
 // app.use(function (err, req, res, next) {
 //   console.error(err.stack)
 //   res.status(500).send('Something broke!')
+// })
+
+// function errorHandler (err, req, res, next) {
+//   if (res.headersSent) {
+//     return next(err)
+//   }
+//   res.status(500)
+//   res.render('error', { error: err })
+// }
+
+// app.use(function (err, req, res, next) {
+//   console.error(err.stack)
+//   res.status(500).send('Something broke!')
+// })
+
+// app.use(function(err, req, res, next) {
+//   console.log(res);
+//   // logger.error('Error rendering route', {
+//   //   stack: err.stack,
+//   // })
+
+//   res.redirect('/error')
 // })
 
 // module.export = app
